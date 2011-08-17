3e72fa8939d7581fc565959353c138f2432b8ab4
Pavel Paulau <pavel.paulau@gmail.com>
2011-08-17 20:02:57 +0300

    Add index for labels and timestamps

--- trunk/harstorage/lib/MongoHandler.py
+++ trunk/harstorage/lib/MongoHandler.py
@@ -1,7 +1,12 @@
-from pymongo import objectid, Connection
+from pymongo import objectid, Connection, ASCENDING, DESCENDING
 
 class MongoDB():
     def __init__(self,host='localhost',port=27017):
         connection = Connection(host, port)
         db = connection['harstorage']        
         self.collection = db['results']
+        
+        self.collection.ensure_index([
+            ('label',     1),
+            ('timestamp', -1)
+        ])
