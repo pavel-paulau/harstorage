29a1c0bd30e2b237be645f956a82a8991c2c8cbe
Pavel Paulau <pavel.paulau@gmail.com>
2011-09-21 21:22:47 +0300

    Added reordering to tables

--- harstorage/public/scripts/hs-ui.js
+++ harstorage/public/scripts/hs-ui.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $('#stats_table').dataTable({
         "bJQueryUI": true,
-        "sPaginationType": "full_numbers"
+        "sPaginationType": "full_numbers",
+        "sDom": 'R<"H"lfr>t<"F"ip<',
     });
 });
