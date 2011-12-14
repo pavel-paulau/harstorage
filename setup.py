e3d6e28b950c0254c6d1e31fdec60cb7f24a6697
Pavel Paulau <Pavel.Paulau@gmail.com>
2011-12-14 10:03:04 +0300

    Changed package info

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name='harstorage',
-    version='0.5.1',
+    version='0.5.2',
     description='Free repository for performance measurements',
     long_description="HAR Storage is repository for automated client-side performance testing. It's built on MongoDB and Pylons.",
     author='Pavel Paulau',
