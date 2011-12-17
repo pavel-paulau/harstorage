7f964afaaf0b821e7986d40425f2c1565d09b71d
Pavel Paulau <pavel.paulau@gmail.com>
2011-12-17 14:53:49 +0200

    Added mongodb hostname and post options

--- harstorage/lib/MongoHandler.py
+++ harstorage/lib/MongoHandler.py
@@ -1,15 +1,20 @@
 import pymongo
 
+from pylons import config
+
 class MongoDB():
 
     """
     Interface for MongoDB database
     """
 
-    def __init__(self, host='localhost', port=27017):
+    def __init__(self):
         """Initilize connection and check indeces"""
 
         # Connection handler
+        host = config['app_conf']['mongo_host']
+        port = int( config['app_conf']['mongo_port'] )
+
         connection = pymongo.Connection(host, port)
         db = connection['harstorage']        
         self.collection = db['results']
