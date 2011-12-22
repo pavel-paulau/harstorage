ca26e0e0db5f57af5f254c5102714a88f4c6c446
Pavel Paulau <Pavel.Paulau@gmail.com>
2011-12-22 17:49:41 +0300

    Changed build configuration

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from ez_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup, find_packages
+from setuptools import setup, find_packages
 
 setup(
     name='harstorage',
@@ -14,19 +9,15 @@ setup(
     author_email='Pavel.Paulau@gmail.com',
     url='http://harstorage.com/',
     license='BSD, see LICENSE.txt for details',
-    install_requires=[
-    ],
-    setup_requires=["PasteScript>=1.6.3"],
-    packages=find_packages(exclude=['ez_setup']),
+	platforms=['Linux', 'Windows'],
+    setup_requires=[],
+	install_requires=[],
+    packages=find_packages(),
     include_package_data=True,
     test_suite='nose.collector',
     package_data={'harstorage': ['i18n/*/LC_MESSAGES/*.mo']},
-    #message_extractors={'harstorage': [
-    #        ('**.py', 'python', None),
-    #        ('templates/**.mako', 'mako', {'input_encoding': 'utf-8'}),
-    #        ('public/**', 'ignore', None)]},
     zip_safe=False,
-    paster_plugins=['PasteScript', 'Pylons'],
+    paster_plugins=[],
     entry_points="""
     [paste.app_factory]
     main = harstorage.config.middleware:make_app
