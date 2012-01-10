949acf7515be09e9527f181330735b51fa6a671b
Pavel.Paulau@gmail.com <Pavel.Paulau@gmail.com>
2012-01-10 14:05:13 +0300

    Refactored code style

--- harstorage/public/scripts/preferences.js
+++ harstorage/public/scripts/preferences.js
@@ -20,7 +20,7 @@ HARSTORAGE.read_cookie = function(name) {
     var nameEQ = name + "=";
     var cookies = document.cookie.split(';');
 
-    for(var i=0;i < cookies.length;i++) {
+    for (var i=0; i < cookies.length; i++) {
         var cookie = cookies[i];
         while (cookie.charAt(0) === ' ') {
             cookie = cookie.substring(1, cookie.length);
