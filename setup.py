53fea7a967e19c459ac62faf993157eccc3ae545
Pavel Paulau <pavel.paulau@gmail.com>
2012-03-03 13:31:31 +0300

    Updated package info

--- setup.py
+++ setup.py
@@ -2,22 +2,22 @@ from setuptools import setup, find_packages
 
 setup(
     name='harstorage',
-    version='0.6',
+    version='1.0b',
     description='Free repository for performance measurements',
     long_description="HAR Storage is repository for automated client-side performance testing. It's built on MongoDB and Pylons.",
     author='Pavel Paulau',
     author_email='Pavel.Paulau@gmail.com',
     url='http://harstorage.com/',
     license='BSD, see LICENSE.txt for details',
-	platforms=['Linux', 'Windows'],
+    platforms=['Linux', 'Windows'],
     setup_requires=[],
-	install_requires=[],
+    install_requires=[],
     packages=find_packages(),
     include_package_data=True,
     test_suite='nose.collector',
     package_data={'harstorage': ['i18n/*/LC_MESSAGES/*.mo']},
     zip_safe=False,
-    paster_plugins=[],
+    paster_plugins=['PasteScript', 'Pylons'],
     entry_points="""
     [paste.app_factory]
     main = harstorage.config.middleware:make_app
