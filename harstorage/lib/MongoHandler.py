301fb2c7a0879e5a6576cf0189fe6c8e6483cc14
Pavel.Paulau@gmail.com <Pavel.Paulau@gmail.com>
2012-03-16 15:40:29 +0300

    Refactored MongoDB handler

--- harstorage/lib/MongoHandler.py
+++ harstorage/lib/MongoHandler.py
@@ -12,32 +12,38 @@ class MongoDB():
         """Initilize connection and check indeces"""
 
         try:
-            # Connection handler
-            host = config["app_conf"]["mongo_host"]
-            port = config["app_conf"]["mongo_port"]
-            auth = config["app_conf"]["mongo_auth"]
+            # MongoDB URI
+            uri = self.make_uri()
 
-            uri = "mongodb://"
-
-            if auth == "true":
-                user = config["app_conf"]["mongo_user"]
-                pswd = config["app_conf"]["mongo_pswd"]
-                uri += user + ":" + pswd + "@"
-
-            uri += host + ":" + port
-
+            # Database
             database = config["app_conf"]["mongo_db"]
 
+            # Collection
             self.collection = pymongo.Connection(uri, safe=True)[database][collection]
 
             # Indecies
-            self.collection.ensure_index([("label", 1), ("timestamp", -1)])
-
-            self.collection.ensure_index([("label", 1), ("timestamp", 1)])
-
-            self.collection.ensure_index([("url", 1), ("timestamp", -1)])
-
-            self.collection.ensure_index([("url", 1), ("timestamp", 1)])            
+            self.ensure_index()
         except Exception as error:
             # Exception type: Exception message
             c.message = ": ".join([type(error).__name__, error.message])
+
+    def make_uri(self):
+        # Connection handler
+        host = config["app_conf"]["mongo_host"]
+        port = config["app_conf"]["mongo_port"]
+        auth = config["app_conf"]["mongo_auth"]
+
+        uri = "mongodb://"
+
+        if auth == "true":
+            user = config["app_conf"]["mongo_user"]
+            pswd = config["app_conf"]["mongo_pswd"]
+            uri += user + ":" + pswd + "@"
+
+        return uri + host + ":" + port
+
+    def ensure_index(self):
+        self.collection.ensure_index([("label", 1), ("timestamp", -1)])
+        self.collection.ensure_index([("label", 1), ("timestamp", 1)])
+        self.collection.ensure_index([("url", 1), ("timestamp", -1)])
+        self.collection.ensure_index([("url", 1), ("timestamp", 1)])
