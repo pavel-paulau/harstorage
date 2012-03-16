f5b479b2e5a3bc664854ea5b527c82aedbc3947d
Pavel.Paulau@gmail.com <Pavel.Paulau@gmail.com>
2012-03-16 15:28:28 +0300

    Refactored HAR library

--- harstorage/lib/HAR.py
+++ harstorage/lib/HAR.py
@@ -150,181 +150,98 @@ class HAR():
                 self.origin = har
 
                 # Initial varaibles and counters
-                self.full_load_time = 0
-
-                self.total_dns_time      = 0.0
-                self.total_transfer_time = 0.0
-                self.total_server_time   = 0.0
-                self.avg_connecting_time = 0.0
-                self.avg_blocking_time   = 0.0
-
-                self.total_size = Bytes(0)
-                self.text_size  = Bytes(0)
-                self.media_size = Bytes(0)
-                self.cache_size = Bytes(0)
-
-                self.redirects    = 0
-                self.bad_requests = 0
-
-                self.domains = dict()
+                self.init_variables()
 
                 self.parsing_status = "Successful"
 
             except Exception as error:
                 self.parsing_status = error
 
+    def init_variables(self):
+        self.full_load_time = 0
+
+        self.total_dns_time      = 0.0
+        self.total_transfer_time = 0.0
+        self.total_server_time   = 0.0
+        self.avg_connecting_time = 0.0
+        self.avg_blocking_time   = 0.0
+
+        self.total_size = Bytes(0)
+        self.text_size  = Bytes(0)
+        self.media_size = Bytes(0)
+        self.cache_size = Bytes(0)
+
+        self.redirects    = 0
+        self.bad_requests = 0
+
+        self.domains = dict()
+
     def analyze(self):
         """Extract data from HAR container"""
 
         # Temporary extremes
-        min_timestamp = 10 ** 14
-        max_timestamp = 0
+        self.min_timestamp = 10 ** 14
+        self.max_timestamp = 0
 
         # Parse each entry of page
-        for entry in self.har["log"]["entries"]:
-            # Detailed timgings
-            dns_time        = max(entry["timings"]["dns"], 0)
-            transfer_time   = entry["timings"]["receive"] + entry["timings"]["send"]
-            transfer_time   = max(transfer_time, 0)
-            server_time     = max(entry["timings"]["wait"], 0)
-            connecting_time = max(entry["timings"]["connect"], 0)
-            blocking_time   = max(entry["timings"]["blocked"], 0)
+        for self.entry in self.har["log"]["entries"]:
+            # Micro timgings
+            self.total_dns_time      += self.get_dns_time()
+            self.total_transfer_time += self.get_transfer_time()
+            self.total_server_time   += self.get_server_time()
+            self.avg_connecting_time += self.get_connecting_time()
+            self.avg_blocking_time   += self.get_blocking_time()
 
-            self.total_dns_time      += dns_time
-            self.total_transfer_time += transfer_time
-            self.total_server_time   += server_time
-            self.avg_connecting_time += connecting_time
-            self.avg_blocking_time   += blocking_time
+            # Update Request/Page time frame
+            self.update_timeframe()
 
-            # Original time format: 2000-01-01T00:00:00.000+00:00
-            seconds, dot, milliseconds = entry["startedDateTime"].partition(".")
-            seconds = time.strptime(seconds, "%Y-%m-%dT%H:%M:%S")
-            seconds = time.mktime(seconds)
-
-            # Exclude timezone
-            try:
-                milliseconds = milliseconds.partition("+")[0]
-            except:
-                milliseconds = milliseconds.partition("-")[0]
-            milliseconds = milliseconds.replace("Z", "")
-
-            time_request_started = seconds + float("0." + milliseconds)
-            time_request_completed = time_request_started + entry["time"]/1000.0
-    
-            if time_request_started < min_timestamp:
-                min_timestamp = time_request_started
-
-                self.time_to_first_byte = blocking_time + \
-                                          dns_time + \
-                                          connecting_time + \
-                                          entry["timings"]["send"] + \
-                                          server_time
-
-            if time_request_completed > max_timestamp:
-                max_timestamp = time_request_completed
+            # Time to first byte
+            self.time_to_first_byte = self.get_time_to_first_byte()
 
             # Size of response body
-            compressed_size = Bytes(max(entry["response"]["bodySize"], 0))
-            if compressed_size == 0:
-                response_size = Bytes(entry["response"]["content"]["size"])
-            else:
-                response_size = compressed_size
-
-            self.total_size += response_size
+            self.total_size += self.get_response_size()
 
             # Size of text (JavaScript, CSS, HTML, XML, JSON, plain text)
             # and media (images, flash) files
-            mime_type = entry["response"]["content"]["mimeType"].partition(";")[0]
-
-            if cmp(mime_type, ""):
-                mime_type = self.get_normalized_value(mime_type)
-
-                if mime_type.count("javascript") \
-                or mime_type.count("text") \
-                or mime_type.count("html") \
-                or mime_type.count("xml") \
-                or mime_type.count("json"):
-                    self.text_size += response_size
-                elif mime_type.count("flash") or mime_type.count("image"):
-                    self.media_size += response_size
+            if self.is_text():
+                self.text_size += self.get_response_size()
+            elif self.is_media():
+                self.media_size += self.get_response_size()
 
             # Cached size
-            headers = Headers(entry["response"]["headers"])
-
-            try:
-                cache_control = headers.as_dict["Cache-Control"]
-
-                if not cache_control.count("no-cache") \
-                and not cache_control.count("max-age=0"):
-
-                    # Extract DATE from HTTP header
-                    date = headers.as_dict["Date"]
-                    date = time.strptime(date, DATE_FORMAT)
-                    date = time.mktime(date)
-
-                    # Extract EXPIRES from HTTP header
-                    expires = headers.as_dict["Expires"]
-                    expires = time.strptime(expires, DATE_FORMAT)
-                    expires = time.mktime(expires)
-
-                    if expires > date:
-                        self.cache_size += response_size
-            except:
-                pass
+            if self.is_long_term_cache():
+                self.cache_size += self.get_response_size()
 
             # Redirects (3xx) and bad requests (4xx, 5xx)
-            if entry["response"]["status"] >= 300 and entry["response"]["status"] < 400:
+            if self.is_redirect():
                 self.redirects += 1
-            elif entry["response"]["status"] >= 400:
+            elif self.is_bad_request():
                 self.bad_requests += 1
 
-            # Current domain
-            domain = entry["request"]["url"].partition("//")[-1].partition("/")[0]
-
-            # WORKAROUND: Mongo prevents using dots in key names
-            mongo_domain = re.sub("\.","|", domain)
-
-            # {DOMAIN: [NUMBER OF REQUESTS, TOTAL DATA FROM HOST IN KB], ...}
-            domain_requests  = self.domains.get(mongo_domain, [0, 0])[0]
-            domain_data_size = self.domains.get(mongo_domain, [0, 0])[1]
-
-            domain_requests  += 1
-            domain_data_size += response_size.to_kilobytes()
-
-            self.domains[mongo_domain] = [domain_requests, domain_data_size]
+            # Update domain info
+            self.update_domain_info()
 
         # Label
-        self.label = self.har["log"]["pages"][0]["id"]
+        self.label = self.get_label()
 
         # URL
-        self.url = self.har["log"]["entries"][0]["request"]["url"]
+        self.url = self.get_url()
 
         # Requests
-        self.requests = len( self.har["log"]["entries"] )
+        self.requests = self.get_number_of_requests()
 
         # Full load time
-        try:
-            self.full_load_time = self.har["log"]["pages"][0]["pageTimings"]["_myTime"]
-        except:
-            self.full_load_time = int((max_timestamp - min_timestamp) * 1000)
+        self.full_load_time = self.get_full_load_time()
 
         # onLoad envent time
-        try:
-            self.onload_event = self.har["log"]["pages"][0]["pageTimings"]["onLoad"]
-        except KeyError:
-            self.onload_event = "n/a"
-        except TypeError: # dynaTrace bug
-            self.onload_event = self.har["log"]["pages"][0]["pageTimings"][0]["onLoad"]
+        self.onload_event = self.get_onload_event()
 
         # Render Start
-        try:
-            self.start_render_time = self.har["log"]["pages"][0]["pageTimings"]["_renderStart"]
-        except:
-            self.start_render_time = "n/a"
+        self.start_render_time = self.get_start_render_time()
 
         # Average values
-        self.avg_connecting_time = round(self.avg_connecting_time / self.requests, 0)
-        self.avg_blocking_time = round(self.avg_blocking_time / self.requests, 0)
+        self.avg_connecting_time = self.get_avg_connecting_time()
+        self.avg_blocking_time = self.get_avg_blocking_time()
 
         # From bytes to kilobytes
         self.total_size = self.total_size.to_kilobytes()
@@ -382,3 +299,180 @@ class HAR():
             return "json"
         else:
             return "other"
+
+    def get_dns_time(self):
+        dns_time = self.entry["timings"]["dns"]
+        return max(dns_time, 0)
+
+    def get_transfer_time(self):
+        transfer_time = self.entry["timings"]["receive"] + \
+                        self.entry["timings"]["send"]
+        return max(transfer_time, 0)
+
+    def get_send_time(self):
+        send_time = self.entry["timings"]["send"]
+        return max(send_time, 0)
+
+    def get_server_time(self):
+        server_time = self.entry["timings"]["wait"]
+        return max(server_time, 0)
+    
+    def get_connecting_time(self):
+        connecting_time = self.entry["timings"]["connect"]
+        return max(connecting_time, 0)
+    
+    def get_blocking_time(self):
+        blocking_time = self.entry["timings"]["blocked"]
+        return max(blocking_time, 0)
+
+    def update_timeframe(self):
+        # Original time format: 2000-01-01T00:00:00.000+00:00
+        seconds, dot, milliseconds = self.entry["startedDateTime"].partition(".")
+        seconds = time.strptime(seconds, "%Y-%m-%dT%H:%M:%S")
+        seconds = time.mktime(seconds)
+
+        # Exclude timezone
+        try:
+            milliseconds = milliseconds.partition("+")[0]
+        except:
+            milliseconds = milliseconds.partition("-")[0]
+        milliseconds = milliseconds.replace("Z", "")
+
+        time_request_started = seconds + float("0." + milliseconds)
+        time_request_completed = time_request_started + self.entry["time"]/1000.0
+
+        if time_request_completed > self.max_timestamp:
+            self.max_timestamp = time_request_completed
+
+        if time_request_started < self.min_timestamp:
+            self.min_timestamp = time_request_started
+            self.is_first = True
+        else:
+            self.is_first = False
+            
+
+    def get_time_to_first_byte(self):
+        if self.is_first:
+            return self.get_blocking_time() + \
+                   self.get_dns_time() + \
+                   self.get_connecting_time() + \
+                   self.get_send_time() + \
+                   self.get_server_time()
+        else:
+            return self.time_to_first_byte
+
+    def get_response_size(self):
+        compressed_size = Bytes(max(self.entry["response"]["bodySize"], 0))
+        if compressed_size == 0:
+            return Bytes(self.entry["response"]["content"]["size"])
+        else:
+            return compressed_size
+
+    def is_text(self):
+        mime_type = self.entry["response"]["content"]["mimeType"].partition(";")[0]
+        if cmp(mime_type, ""):
+            mime_type = self.get_normalized_value(mime_type)
+
+            if mime_type.count("javascript") \
+            or mime_type.count("text") \
+            or mime_type.count("html") \
+            or mime_type.count("xml") \
+            or mime_type.count("json"):
+                return True
+        return False
+
+    def is_media(self):
+        mime_type = self.entry["response"]["content"]["mimeType"].partition(";")[0]
+
+        if cmp(mime_type, ""):
+            mime_type = self.get_normalized_value(mime_type)
+            if mime_type.count("flash") or mime_type.count("image"):
+                return True
+        return False
+
+    def is_long_term_cache(self):
+        headers = Headers(self.entry["response"]["headers"])
+
+        try:
+            cache_control = headers.as_dict["Cache-Control"]
+
+            if not cache_control.count("no-cache") \
+            and not cache_control.count("max-age=0"):
+
+                # Extract DATE from HTTP header
+                date = headers.as_dict["Date"]
+                date = time.strptime(date, DATE_FORMAT)
+                date = time.mktime(date)
+
+                # Extract EXPIRES from HTTP header
+                expires = headers.as_dict["Expires"]
+                expires = time.strptime(expires, DATE_FORMAT)
+                expires = time.mktime(expires)
+
+                if expires > date:
+                    return True
+        except:
+            pass
+        return False
+
+    def is_redirect(self):
+        if self.entry["response"]["status"] >= 300 \
+        and self.entry["response"]["status"] < 400:
+            return True
+
+    def is_bad_request(self):
+        if self.entry["response"]["status"] >= 400:
+            return True
+
+    def get_label(self):
+        return self.har["log"]["pages"][0]["id"]
+
+    def get_url(self):
+        return self.har["log"]["entries"][0]["request"]["url"]
+
+    def get_number_of_requests(self):
+        return len(self.har["log"]["entries"])
+
+    def get_full_load_time(self):
+        try:
+            return self.har["log"]["pages"][0]["pageTimings"]["_myTime"]
+        except:
+            return int((self.max_timestamp - self.min_timestamp) * 1000)
+
+    def get_onload_event(self):
+        try:
+            onload_event = self.har["log"]["pages"][0]["pageTimings"]["onLoad"]
+            if onload_event > 0:
+                return onload_event                
+        except TypeError: # dynaTrace bug
+            return self.har["log"]["pages"][0]["pageTimings"][0]["onLoad"]
+        except KeyError:
+            pass        
+        return "n/a"
+
+    def get_start_render_time(self):
+        try:
+            return self.har["log"]["pages"][0]["pageTimings"]["_renderStart"]
+        except:
+            return "n/a"
+
+    def get_avg_connecting_time(self):
+        return round(self.avg_connecting_time / self.requests, 0)        
+
+    def get_avg_blocking_time(self):
+        return round(self.avg_blocking_time / self.requests, 0)
+
+    def update_domain_info(self):
+        domain = self.entry["request"]["url"].partition("//")[-1].partition("/")[0]
+
+        # WORKAROUND: Mongo prevents using dots in key names
+        mongo_domain = re.sub("\.","|", domain)
+
+        # {DOMAIN: [NUMBER OF REQUESTS, TOTAL DATA FROM HOST IN KB], ...}
+        domain_requests  = self.domains.get(mongo_domain, [0, 0])[0]
+        domain_data_size = self.domains.get(mongo_domain, [0, 0])[1]
+
+        domain_requests  += 1
+        domain_data_size += self.get_response_size().to_kilobytes()
+
+        self.domains[mongo_domain] = [domain_requests, domain_data_size]
